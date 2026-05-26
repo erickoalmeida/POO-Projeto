46e51012ba5b8a19a0a28840af1191212422fe66
erickoalmeida <113539224+erickoalmeida@users.noreply.github.com>
2026-05-26 17:26:23 -0300

    Implement inheritance for Player and Pocao classes
    Refactor Player and Pocao classes to inherit from Players and PocoesIlegais respectively.

--- models.py
+++ models.py
@@ -1,13 +1,30 @@
-class Player:
+'''
+Heranca
+Quando eu quiser aproveitar atributos ou metodos entre classes 
+eu uso o conceito de Heranca
+'''
+'''
+ "Um player tem coisas ('ativo', 'nome') ent, ele pode ter coisas de 
+ Pocao"
+'''
+class Players:
     def __init__(self, nome):
         self.nome = nome
         self.ativo = True
 
-class Pocao:
+class PocoesIlegais:
     def __init__(self, nome, tipo):
         self.nome = nome
         self.tipo = tipo
         self.ativo = True
+
+class Player(Players):
+    def __init__(self, nome):
+        super().__init__(nome)
+
+class Pocao(PocoesIlegais):
+    def __init__(self, nome, tipo):
+        super().__init__(nome, tipo)
 
 class Jogo:
     def __init__(self):
