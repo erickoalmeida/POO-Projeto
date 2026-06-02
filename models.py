685480c5688130fa53597a775dcdf2671650844f
erickoalmeida <113539224+erickoalmeida@users.noreply.github.com>
2026-06-02 08:27:25 -0300

    Refactor Player and Pocao classes
    Refactor classes to remove inheritance and simplify attributes.

--- models.py
+++ models.py
@@ -1,29 +1,13 @@
-'''
-Heranca
-Quando eu quiser aproveitar atributos ou metodos entre classes 
-eu uso o conceito de Heranca
- "Um player tem coisas ('ativo', 'nome') ent, ele pode ter coisas de 
- Pocao"
- "nao tem, no super, 'ativo', pois ja utilizamos nas duas classes"
-'''
-class Players:
+class Player:
     def __init__(self, nome):
-        self._nome = nome
-        self._ativo = True 
+        self.nome = nome
+        self.ativo = True
 
-class PocoesIlegais:
+class Pocao:
     def __init__(self, nome, tipo):
-        self._nome = nome
-        self._tipo = tipo
-        self._ativo = True
-
-class Player(Players):
-    def __init__(self, nome):
-        super().__init__(nome)
-
-class Pocao(PocoesIlegais):
-    def __init__(self, nome, tipo):
-        super().__init__(nome, tipo)
+        self.nome = nome
+        self.tipo = tipo
+        self.ativo = True
 
 class Jogo:
     def __init__(self):
